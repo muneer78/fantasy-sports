a95fc93adf136777d603f44f83955309cc0679bf
muneer78 <muneer@duck.com>
2022-12-16 16:14:37 -0600

    Trying to drop columns

---
+++ baseballwithdrop.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def percent_to_float(s):
+  float(stringPercent[:-1]) / 100
+
+df = pd.read_csv("fangraphs.csv", index_col=["playerid"])
+
+df.columns = df.columns.str.replace('[+,-,%,]', '')
+df.rename(columns={'K-BB':'KMinusBB','K/BB':'KToBB', 'HR/9':'HRPer9', 'xFIP-':'XFIPMinus'}, inplace=True)
+df.fillna(0)
+
+df['KMinusBB'] = df['KMinusBB'] = df['KMinusBB'].str.rstrip('%').astype('float')
+df['Barrel'] = df['Barrel'] = df['Barrel'].str.rstrip('%').astype('float')
+df['CSW'] = df['CSW'] = df['CSW'].str.rstrip('%').astype('float')
+
+filters1 = df[(df['xERA'] < 3) & (df['Barrel'] < 5) & (df['CSW'] > 20) & (df['KMinusBB'] > 20) & (df['Starting'] > 5)] 
+filters2 = df[(df['xERA'] < 3) & (df['Barrel'] < 5) & (df['CSW'] > 20) & (df['KMinusBB'] > 30) & (df['Relieving'] > 1)] 
+
+print(filters1.drop(['Relieving'],axis=1))
+print(filters2.drop(['Starting'],axis=1))
+
+filters1.to_excel("pitchingSP.xlsx", sheet_name='StartersSeason')
+filters2.to_excel("pitchingRP.xlsx", sheet_name='RelieversSeason')
