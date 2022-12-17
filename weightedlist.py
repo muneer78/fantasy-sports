0d2fdf528b86c1d057e246a68e1b234bb40d964f
muneer78 <muneer@duck.com>
2022-12-17 14:59:38 -0600

    deleted unneeded scripts

--- weightedlist.py
+++
@@ -1,8 +0,0 @@
-import random
-
-teams = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th']
-
-randomList = random.shuffle(
-  teams, weights=(1, 2, 4, 8, 16, 32, 64, 128, 256, 512), k=10)
-  
-print(randomList)
