401df6b3f537127990e2678ae5d0effd40322a35
muneer78 <muneer@duck.com>
2022-12-16 21:39:18 -0600

    still no luck

--- weightedlist.py
+++ weightedlist.py
@@ -2,7 +2,7 @@ import random
 
 teams = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th']
 
-randomList = random.choices(
-  teams, weights=(1, 2, 4, 8, 16, 32, 64, 128, 256. 512, k=10)
+randomList = random.shuffle(
+  teams, weights=(1, 2, 4, 8, 16, 32, 64, 128, 256, 512), k=10)
   
 print(randomList)
