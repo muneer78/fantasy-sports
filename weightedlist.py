e31cae5402e08b4fe2f4faf5b1517451cc0e9000
muneer78 <muneer@duck.com>
2022-12-16 21:34:49 -0600

    attempts at weighted lists

---
+++ weightedlist.py
@@ -0,0 +1,8 @@
+import random
+
+teams = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th']
+
+randomList = random.choices(
+  teams, weights=(1, 2, 4, 8, 16, 32, 64, 128, 256. 512, k=10)
+  
+print(randomList)
