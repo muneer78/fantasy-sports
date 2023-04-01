9998f1515fbd7e826b30234adf48a62b7a47bcb8
muneer78 <muneer@duck.com>
2023-04-01 15:31:30 -0500

    Created weekly Fangraphs file processor.

---
+++ weeklybaseball.py
@@ -0,0 +1,75 @@
+import pandas as pd
+from datetime import date, datetime, timedelta
+
+#define dictionary
+some_dict = {'FanGraphs Leaderboard.csv': 'fgl_pitchers_10_ip.csv',
+             'FanGraphs Leaderboard(1).csv': 'fgl_pitchers_30_ip.csv',
+             'FanGraphs Leaderboard(2).csv': 'fgl_hitters_40_pa.csv',
+             'FanGraphs Leaderboard(3).csv': 'fgl_hitters_last_14.csv',
+             'FanGraphs Leaderboard(4).csv': 'fgl_hitters_last_7.csv',
+             'FanGraphs Leaderboard(5).csv': 'fgl_pitchers_last_14.csv',
+             'FanGraphs Leaderboard(6).csv': 'fgl_pitchers_last_30.csv','
+             }
+
+#today = date.today()
+today = datetime.strptime('2023-10-31', '%Y-%m-%d').date() # pinning to last day of baseball season
+
+
+def hitters_preprocessing(filepath):
+    df = pd.read_csv(filepath, index_col=["playerid"])
+
+    df.columns = df.columns.str.replace('[+,-,%,]', '')
+    df.rename(columns={'K%-': 'K', 'BB%-': 'BB'}, inplace=True)
+    df.fillna(0)
+
+    df['Barrel'] = df['Barrel'] = df['Barrel'].str.rstrip('%').astype('float')
+
+    filters = df[(df['wRC'] > 135) & (df['OPS'] > .8) & (df['K'] < 95) & (df['BB'] > 100) & (df['Off'] > 1) & (
+                df['Barrel'] > 10)].sort_values(by='Off', ascending=False)
+    print(filters.head())
+    return filters
+    # Prepare the workbook for adding new sheets
+    path = "fg_analysis.xlsx"
+
+def pitchers_preprocessing(filepath):
+    df = pd.read_csv(filepath, index_col=["playerid"])
+
+    df.columns = df.columns.str.replace('[+,-,%,]', '')
+    df.rename(columns={'K/BB': 'KToBB', 'HR/9': 'HRPer9', 'xFIP-': 'XFIPMinus'}, inplace=True)
+    df.fillna(0)
+
+    df['Barrel'] = df['Barrel'] = df['Barrel'].str.rstrip('%').astype('float')
+    df['CSW'] = df['CSW'] = df['CSW'].str.rstrip('%').astype('float')
+
+    filters1 = df[(df['Barrel'] < 7) & (df['Starting'] > 5) & (df['GS'] > 1)].sort_values(by='Starting', ascending=False)
+    filters2 = df[(df['Barrel'] < 7) & (df['Relieving'] > 1)].sort_values(by='Relieving', ascending=False)
+    return filters1
+    return filters2
+    # Prepare the workbook for adding new sheets
+    path = "fg_analysis.xlsx"
+
+# Preprocess and export the dataframes to Excel workbook sheets
+hitdaywindow = [7, 14]
+pawindow = [40]
+pitchdaywindow = [14, 30]
+ipwindow = [10, 30]
+
+with pd.ExcelWriter(path, engine="openpyxl", mode="w") as writer:
+    for w in daywindow:
+        df = hitters_preprocessing(f'fgl_hitters_last_{w}.csv')
+        df.to_excel(writer, sheet_name=f'Hitters Last {w} Days', index=False)
+        df.columns
+    writer.close()
+
+    for w in pawindow:
+        df = hitters_preprocessing(f'fgl_hitters_{w}_pa.csv')
+        df.to_excel(writer, sheet_name=f'Hitters {w} PA', index=False)
+        df.columns
+    writer.close()
+
+    for w in pitchdaywindow:
+        df = pitchers_preprocessing(f'fgl_pitchers_last_{w}.csv')
+        df.to_excel(writer, sheet_name=f'Hitters {w} PA', index=False)
+        df.columns
+    writer.close()
+
