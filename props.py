0b576ec8ed62de915997914b7885aec3b4f7f503
muneer78 <muneer@duck.com>
2023-08-13 09:48:08 -0500

    Now generating props for today and tomorrow and not writing a dataframe to output if there's no data.

--- props.py
+++ props.py
@@ -17,13 +17,11 @@ dfprops['IP/G'] = dfprops['IP/G'].round(0)
 
 # Filter rows where GS > 1
 dfprops = dfprops.loc[(dfprops['GS'] > 1) & (dfprops['IP/G'] > 5)]
-# dfprops = dfprops.loc[dfprops['GS'] > 1]
-print(dfprops)
 
 dfsched.rename(columns={dfsched.columns[2]: "Name"}, inplace=True)
 dfsched = dfsched.fillna(0)
 
-# Delete the second column
+# Delete the second column from dfsched
 dfsched = dfsched.drop(dfsched.columns[1], axis=1)
 
 dfopponents = pd.read_csv(
@@ -48,7 +46,7 @@ dfsched["OpposingTeam"] = opposing_teams
 dfsched["OpposingTeam"] = dfsched["OpposingTeam"].str.replace("@ ", "")
 
 """
-Deletes first line in cell from roster resourec file
+Deletes first line in cell from roster resource file
 """
 def delete_first_line(cell_value):
     lines = cell_value.split("\n")
@@ -71,9 +69,7 @@ def remove_text(cell_value):
 
 dfsched = dfsched.applymap(remove_text)
 
-dfsched = pd.merge(
-    dfsched, dfopponents[["Team", "KRank", "BBRank"]], on="Team", how="left"
-)
+merged_dfsched = pd.merge(dfsched, dfopponents[["Team", "KRank", "BBRank"]], on="Team", how="left")
 
 dfsortedBB = dfprops.sort_values("BB%+", ascending=True)
 dfbbtop25 = dfsortedBB.head(25).fillna(0)
@@ -81,14 +77,14 @@ dfbbtop25 = dfsortedBB.head(25).fillna(0)
 dfsortedK = dfprops.sort_values("K%+", ascending=False)
 dfktop25 = dfsortedK.head(25).fillna(0)
 
-matching_dfbbtop25 = dfsched[dfsched["Name"].isin(dfbbtop25["Name"])]
-matching_dfktop25 = dfsched[dfsched["Name"].isin(dfktop25["Name"])]
+matching_dfbbtop25 = merged_dfsched[merged_dfsched["Name"].isin(dfbbtop25["Name"])]
+matching_dfktop25 = merged_dfsched[merged_dfsched["Name"].isin(dfktop25["Name"])]
 
-matching_dfbbtop25 = dfsched[
-    (dfsched["Name"].isin(dfbbtop25["Name"])) & ((dfsched["BBRank"] <= 10))
+matching_dfbbtop25 = merged_dfsched[
+    (merged_dfsched["Name"].isin(dfbbtop25["Name"])) & ((merged_dfsched["BBRank"] <= 10))
 ]
-matching_dfktop25 = dfsched[
-    (dfsched["Name"].isin(dfktop25["Name"])) & ((dfsched["KRank"] <= 10))
+matching_dfktop25 = merged_dfsched[
+    (merged_dfsched["Name"].isin(dfktop25["Name"])) & ((merged_dfsched["KRank"] <= 10))
 ]
 
 matching_dfktop25 = pd.merge(
@@ -101,15 +97,128 @@ matching_dfbbtop25 = pd.merge(
 matching_dfktop25 = matching_dfktop25.sort_values(by="K%+", ascending=False)
 matching_dfbbtop25 = matching_dfbbtop25.sort_values(by="BB%+", ascending=True)
 
-list_of_dfs = [matching_dfktop25, matching_dfbbtop25]
+'''
+Code to show today's plays
+'''''
+
+dfsched2 = pd.read_excel("roster-resource-download.xlsx", usecols=[0, 1, 2])
+dfprops2 = pd.read_csv("FanGraphs Leaderboard.csv")
+
+dfprops2['K/GS'] = dfprops2['SO'] / dfprops2['GS']
+dfprops2 ['K/GS' ] = dfprops2['K/GS'].round(2)
+dfprops2['BB/GS'] = dfprops2['BB'] / dfprops2['GS']
+dfprops2['BB/GS'] = dfprops2['BB/GS'].round(2)
+dfprops2['IP/G'] = dfprops2['IP'] / dfprops2['GS']
+dfprops2['IP/G'] = dfprops2['IP/G'].round(0)
+
+# Filter rows where GS > 1
+dfprops2 = dfprops2.loc[(dfprops2['GS'] > 1) & (dfprops2['IP/G'] > 5)]
+
+dfsched2.rename(columns={dfsched2.columns[1]: "Name"}, inplace=True)
+dfsched2 = dfsched2.fillna(0)
+
+dfopponents2 = pd.read_csv(
+    "FanGraphs Leaderboard (1).csv", usecols=["Team", "K%", "BB%"]
+)
+
+dfopponents2["K%"] = dfopponents2["K%"].str.rstrip("%").astype(float)
+dfopponents2["BB%"] = dfopponents2["BB%"].str.rstrip("%").astype(float)
+
+dfopponents2.sort_values("K%", ascending=False, ignore_index=True, inplace=True)
+
+# Add a new column called "KRank"
+dfopponents2["KRank"] = range(1, len(dfopponents) + 1)
+
+
+dfopponents2.sort_values("BB%", ascending=True, ignore_index=True, inplace=True)
+dfopponents2["BBRank"] = range(1, len(dfopponents2) + 1)
+
+dfsched2["Name"] = dfsched2["Name"].str.strip()
+opposing_teams2 = dfsched2.iloc[:, 1].str.split("\n").str[0]
+dfsched2["OpposingTeam"] = opposing_teams
+dfsched2["OpposingTeam"] = dfsched2["OpposingTeam"].str.replace("@ ", "")
+
+"""
+Deletes first line in cell from roster resource file
+"""
+def delete_first_line(cell_value):
+    lines = cell_value.split("\n")
+    if len(lines) > 1:
+        lines.pop(0)
+    return "\n".join(lines)
+
+"""
+Applies delete_first_line function
+"""
+dfsched2 = dfsched2.applymap(delete_first_line)
+
+"""
+Remove the R or L from after pitcher name
+"""
+def remove_text(cell_value):
+    pattern = r"\s*\(R\)|\s*\(L\)"
+    return re.sub(pattern, "", cell_value)
+
+
+dfsched2 = dfsched2.applymap(remove_text)
+
+# Delete the second column from dfsched2
+dfsched2 = dfsched2.drop(dfsched2.columns[2], axis=1)
+
+# Generate dfopponents again
+dfopponents2 = pd.read_csv("FanGraphs Leaderboard (1).csv", usecols=["Team", "K%", "BB%"])
+
+# Update columns in dfopponents2
+dfopponents2["K%"] = dfopponents2["K%"].str.rstrip("%").astype(float)
+dfopponents2["BB%"] = dfopponents2["BB%"].str.rstrip("%").astype(float)
+
+# Sort and add ranking columns
+dfopponents2.sort_values("K%", ascending=False, ignore_index=True, inplace=True)
+dfopponents2["KRank"] = range(1, len(dfopponents2) + 1)
+dfopponents2.sort_values("BB%", ascending=True, ignore_index=True, inplace=True)
+dfopponents2["BBRank"] = range(1, len(dfopponents2) + 1)
+
+# Update merged_merged_dfsched2 with opponent data again
+dfsched2 = pd.merge(dfsched2, dfopponents2[["Team", "KRank", "BBRank"]], on="Team", how="left")
+
+dfsortedBB2 = dfprops2.sort_values("BB%+", ascending=True)
+dfbbtop252 = dfsortedBB2.head(25).fillna(0)
+
+dfsortedK2 = dfprops2.sort_values("K%+", ascending=False)
+dfktop252 = dfsortedK2.head(25).fillna(0)
+
+matching_dfbbtop252 = dfsched2[dfsched2["Name"].isin(dfbbtop252["Name"])]
+matching_dfktop252 = dfsched2[dfsched2["Name"].isin(dfktop252["Name"])]
+
+matching_dfbbtop252 = dfsched2[
+    (dfsched2["Name"].isin(dfbbtop252["Name"])) & ((dfsched2["BBRank"] <= 10))
+]
+matching_dfktop252 = dfsched2[
+    (dfsched2["Name"].isin(dfktop252["Name"])) & ((dfsched2["KRank"] <= 10))
+]
+
+matching_dfktop252 = pd.merge(
+    matching_dfktop252, dfktop252[["Name", "K%+", "BB%+", "xFIP", "K/GS"]], on="Name", how="left"
+)
+matching_dfbbtop252 = pd.merge(
+    matching_dfbbtop252, dfbbtop252[["Name", "K%+", "BB%+", "xFIP", "BB/GS"]], on="Name", how="left"
+)
+
+matching_dfktop252 = matching_dfktop252.sort_values(by="K%+", ascending=False)
+matching_dfbbtop252 = matching_dfbbtop252.sort_values(by="BB%+", ascending=True)
+
+list_of_dfs = [matching_dfktop252, matching_dfbbtop252, matching_dfktop25, matching_dfbbtop25]
 
 titles = [
-    "Pitcher High Strikeout Props",
-    "Pitcher Low BB Props",
+    "Pitcher High Strikeout Props Today",
+    "Pitcher Low BB Props Today",
+    "Pitcher High Strikeout Props Tomorrow",
+    "Pitcher Low BB Props Tomorrow",
 ]
 
 with open("props.csv", "w+", encoding="utf8") as f:
     for i, df in enumerate(list_of_dfs):
-        f.write(titles[i] + "\n")  # Write the title
-        df.to_csv(f, index=False)
-        f.write("\n")
+        if not df.empty:  # Check if the dataframe is not empty
+            f.write(titles[i] + "\n")  # Write the title
+            df.to_csv(f, index=False)
+            f.write("\n")
