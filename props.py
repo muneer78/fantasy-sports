b2e2a89ac8a2793af9461eb306b0ab2b8dd1cd51
muneer78 <muneer@duck.com>
2023-08-22 06:00:19 -0500

    Script updates

--- props.py
+++ props.py
@@ -4,219 +4,220 @@ Provides some arithmetic functions and ability to use regex
 
 import re
 import pandas as pd
+import pandas_log
 
-dfsched = pd.read_excel("roster-resource-download.xlsx", usecols=[0, 1, 2])
-dfprops = pd.read_csv("fangraphs-leaderboards.csv")
+with pandas_log.enable():
+    dfsched = pd.read_excel("roster-resource-download.xlsx", usecols=[0, 1, 2])
+    dfprops = pd.read_csv("fangraphs-leaderboards.csv")
 
-dfprops['K/GS'] = dfprops['SO'] / dfprops['GS']
-dfprops ['K/GS' ] = dfprops['K/GS'].round(2)
-dfprops['BB/GS'] = dfprops['BB'] / dfprops['GS']
-dfprops['BB/GS'] = dfprops['BB/GS'].round(2)
-dfprops['IP/G'] = dfprops['IP'] / dfprops['GS']
-dfprops['IP/G'] = dfprops['IP/G'].round(0)
+    dfprops['K/GS'] = dfprops['SO'] / dfprops['GS']
+    dfprops ['K/GS' ] = dfprops['K/GS'].round(2)
+    dfprops['BB/GS'] = dfprops['BB'] / dfprops['GS']
+    dfprops['BB/GS'] = dfprops['BB/GS'].round(2)
+    dfprops['IP/G'] = dfprops['IP'] / dfprops['GS']
+    dfprops['IP/G'] = dfprops['IP/G'].round(0)
 
-# Filter rows where GS > 1
-dfprops = dfprops.loc[(dfprops['GS'] > 1) & (dfprops['IP/G'] > 5)]
+    # Filter rows where GS > 1
+    dfprops = dfprops.loc[(dfprops['GS'] > 1) & (dfprops['IP/G'] > 5)]
 
-dfsched.rename(columns={dfsched.columns[2]: "Name"}, inplace=True)
-dfsched = dfsched.fillna(0)
+    dfsched.rename(columns={dfsched.columns[2]: "Name"}, inplace=True)
+    dfsched = dfsched.fillna(0)
 
-# Delete the second column from dfsched
-dfsched = dfsched.drop(dfsched.columns[1], axis=1)
+    # Delete the second column from dfsched
+    dfsched = dfsched.drop(dfsched.columns[1], axis=1)
 
-dfopponents = pd.read_csv(
-    "fangraphs-leaderboards (1).csv", usecols=["Team", "K%", "BB%"]
-)
+    dfopponents = pd.read_csv("fangraphs-leaderboards (1).csv", usecols=[ "Team", "K%", "BB%" ]
+    )
 
-# dfopponents["K%"] = dfopponents["K%"].str.rstrip("%").astype(float)
-# dfopponents["BB%"] = dfopponents["BB%"].str.rstrip("%").astype(float)
+    # dfopponents["K%"] = dfopponents["K%"].str.rstrip("%").astype(float)
+    # dfopponents["BB%"] = dfopponents["BB%"].str.rstrip("%").astype(float)
 
-dfopponents.sort_values("K%", ascending=False, ignore_index=True, inplace=True)
+    dfopponents.sort_values("K%", ascending=False, ignore_index=True, inplace=True)
 
-# Add a new column called "KRank"
-dfopponents["KRank"] = range(1, len(dfopponents) + 1)
-
-
-dfopponents.sort_values("BB%", ascending=True, ignore_index=True, inplace=True)
-dfopponents["BBRank"] = range(1, len(dfopponents) + 1)
-
-dfsched["Name"] = dfsched["Name"].str.strip()
-opposing_teams = dfsched.iloc[:, 1].str.split("\n").str[0]
-dfsched["OpposingTeam"] = opposing_teams
-dfsched["OpposingTeam"] = dfsched["OpposingTeam"].str.replace("@ ", "")
-
-"""
-Deletes first line in cell from roster resource file
-"""
-def delete_first_line(cell_value):
-    lines = cell_value.split("\n")
-    if len(lines) > 1:
-        lines.pop(0)
-    return "\n".join(lines)
-
-"""
-Applies delete_first_line function
-"""
-dfsched = dfsched.applymap(delete_first_line)
-
-"""
-Remove the R or L from after pitcher name
-"""
-def remove_text(cell_value):
-    pattern = r"\s*\(R\)|\s*\(L\)"
-    return re.sub(pattern, "", cell_value)
+    # Add a new column called "KRank"
+    dfopponents["KRank"] = range(1, len(dfopponents) + 1)
 
 
-dfsched = dfsched.applymap(remove_text)
+    dfopponents.sort_values("BB%", ascending=True, ignore_index=True, inplace=True)
+    dfopponents["BBRank"] = range(1, len(dfopponents) + 1)
 
-merged_dfsched = pd.merge(dfsched, dfopponents[["Team", "KRank", "BBRank"]], on="Team", how="left")
+    dfsched["Name"] = dfsched["Name"].str.strip()
+    opposing_teams = dfsched.iloc[:, 1].str.split("\n").str[0]
+    dfsched["OpposingTeam"] = opposing_teams
+    dfsched["OpposingTeam"] = dfsched["OpposingTeam"].str.replace("@ ", "")
 
-dfsortedBB = dfprops.sort_values("BB%+", ascending=True)
-dfbbtop25 = dfsortedBB.head(25).fillna(0)
+    """
+    Deletes first line in cell from roster resource file
+    """
+    def delete_first_line(cell_value):
+        lines = cell_value.split("\n")
+        if len(lines) > 1:
+            lines.pop(0)
+        return "\n".join(lines)
 
-dfsortedK = dfprops.sort_values("K%+", ascending=False)
-dfktop25 = dfsortedK.head(25).fillna(0)
+    """
+    Applies delete_first_line function
+    """
+    dfsched = dfsched.applymap(delete_first_line)
 
-matching_dfbbtop25 = merged_dfsched[merged_dfsched["Name"].isin(dfbbtop25["Name"])]
-matching_dfktop25 = merged_dfsched[merged_dfsched["Name"].isin(dfktop25["Name"])]
-
-matching_dfbbtop25 = merged_dfsched[
-    (merged_dfsched["Name"].isin(dfbbtop25["Name"])) & ((merged_dfsched["BBRank"] <= 10))
-]
-matching_dfktop25 = merged_dfsched[
-    (merged_dfsched["Name"].isin(dfktop25["Name"])) & ((merged_dfsched["KRank"] <= 10))
-]
-
-matching_dfktop25 = pd.merge(
-    matching_dfktop25, dfktop25[["Name", "K%+", "BB%+", "xFIP", "K/GS"]], on="Name", how="left"
-)
-matching_dfbbtop25 = pd.merge(
-    matching_dfbbtop25, dfbbtop25[["Name", "K%+", "BB%+", "xFIP", "BB/GS"]], on="Name", how="left"
-)
-
-matching_dfktop25 = matching_dfktop25.sort_values(by="K%+", ascending=False)
-matching_dfbbtop25 = matching_dfbbtop25.sort_values(by="BB%+", ascending=True)
-
-'''
-Code to show today's plays
-'''''
-
-dfsched2 = pd.read_excel("roster-resource-download.xlsx", usecols=[0, 1])
-dfprops2 = pd.read_csv("fangraphs-leaderboards.csv")
-
-dfprops2['K/GS'] = dfprops2['SO'] / dfprops2['GS']
-dfprops2 ['K/GS' ] = dfprops2['K/GS'].round(2)
-dfprops2['BB/GS'] = dfprops2['BB'] / dfprops2['GS']
-dfprops2['BB/GS'] = dfprops2['BB/GS'].round(2)
-dfprops2['IP/G'] = dfprops2['IP'] / dfprops2['GS']
-dfprops2['IP/G'] = dfprops2['IP/G'].round(0)
-
-# Filter rows where GS > 1
-dfprops2 = dfprops2.loc[(dfprops2['GS'] > 1) & (dfprops2['IP/G'] > 5)]
-
-dfsched2.rename(columns={dfsched2.columns[1]: "Name"}, inplace=True)
-dfsched2 = dfsched2.fillna(0)
-
-dfopponents2 = pd.read_csv(
-    "fangraphs-leaderboards (1).csv", usecols=["Team", "K%", "BB%"]
-)
-
-# dfopponents2["K%"] = dfopponents2["K%"].str.rstrip("%").astype(float)
-# dfopponents2["BB%"] = dfopponents2["BB%"].str.rstrip("%").astype(float)
-
-dfopponents2.sort_values("K%", ascending=False, ignore_index=True, inplace=True)
+    """
+    Remove the R or L from after pitcher name
+    """
+    def remove_text(cell_value):
+        pattern = r"\s*\(R\)|\s*\(L\)"
+        return re.sub(pattern, "", cell_value)
 
 
-# Add a new column called "KRank"
-dfopponents2["KRank"] = range(1, len(dfopponents) + 1)
+    dfsched = dfsched.applymap(remove_text)
+
+    merged_dfsched = pd.merge(dfsched, dfopponents[["Team", "KRank", "BBRank"]], on="Team", how="left")
+
+    dfsortedBB = dfprops.sort_values("BB%+", ascending=True)
+    dfbbtop25 = dfsortedBB.head(25).fillna(0)
+
+    dfsortedK = dfprops.sort_values("K%+", ascending=False)
+    dfktop25 = dfsortedK.head(25).fillna(0)
+
+    matching_dfbbtop25 = merged_dfsched[merged_dfsched["Name"].isin(dfbbtop25["Name"])]
+    matching_dfktop25 = merged_dfsched[merged_dfsched["Name"].isin(dfktop25["Name"])]
+
+    matching_dfbbtop25 = merged_dfsched[
+        (merged_dfsched["Name"].isin(dfbbtop25["Name"])) & ((merged_dfsched["BBRank"] <= 10))
+    ]
+    matching_dfktop25 = merged_dfsched[
+        (merged_dfsched["Name"].isin(dfktop25["Name"])) & ((merged_dfsched["KRank"] <= 10))
+    ]
+
+    matching_dfktop25 = pd.merge(
+        matching_dfktop25, dfktop25[["Name", "K%+", "BB%+", "xFIP", "K/GS"]], on="Name", how="left"
+    )
+    matching_dfbbtop25 = pd.merge(
+        matching_dfbbtop25, dfbbtop25[["Name", "K%+", "BB%+", "xFIP", "BB/GS"]], on="Name", how="left"
+    )
+
+    matching_dfktop25 = matching_dfktop25.sort_values(by="K%+", ascending=False)
+    matching_dfbbtop25 = matching_dfbbtop25.sort_values(by="BB%+", ascending=True)
+
+    '''
+    Code to show today's plays
+    '''''
+
+    dfsched2 = pd.read_excel("roster-resource-download.xlsx", usecols=[0, 1])
+    dfprops2 = pd.read_csv("fangraphs-leaderboards.csv")
+
+    dfprops2['K/GS'] = dfprops2['SO'] / dfprops2['GS']
+    dfprops2 ['K/GS' ] = dfprops2['K/GS'].round(2)
+    dfprops2['BB/GS'] = dfprops2['BB'] / dfprops2['GS']
+    dfprops2['BB/GS'] = dfprops2['BB/GS'].round(2)
+    dfprops2['IP/G'] = dfprops2['IP'] / dfprops2['GS']
+    dfprops2['IP/G'] = dfprops2['IP/G'].round(0)
+
+    # Filter rows where GS > 1
+    dfprops2 = dfprops2.loc[(dfprops2['GS'] > 1) & (dfprops2['IP/G'] > 5)]
+
+    dfsched2.rename(columns={dfsched2.columns[1]: "Name"}, inplace=True)
+    dfsched2 = dfsched2.fillna(0)
+
+    dfopponents2 = pd.read_csv(
+        "fangraphs-leaderboards (1).csv", usecols=[ "Team", "K%", "BB%" ]
+    )
+
+    # dfopponents2["K%"] = dfopponents2["K%"].str.rstrip("%").astype(float)
+    # dfopponents2["BB%"] = dfopponents2["BB%"].str.rstrip("%").astype(float)
+
+    dfopponents2.sort_values("K%", ascending=False, ignore_index=True, inplace=True)
 
 
-dfopponents2.sort_values("BB%", ascending=True, ignore_index=True, inplace=True)
-dfopponents2["BBRank"] = range(1, len(dfopponents2) + 1)
-
-dfsched2["Name"] = dfsched2["Name"].str.strip()
-opposing_teams2 = dfsched2.iloc[:, 1].str.split("\n").str[0]
-dfsched2["OpposingTeam"] = opposing_teams2
-dfsched2["OpposingTeam"] = dfsched2["OpposingTeam"].str.replace("@ ", "")
-
-"""
-Deletes first line in cell from roster resource file
-"""
-def delete_first_line(cell_value):
-    lines = cell_value.split("\n")
-    if len(lines) > 1:
-        lines.pop(0)
-    return "\n".join(lines)
-
-"""
-Applies delete_first_line function
-"""
-dfsched2 = dfsched2.applymap(delete_first_line)
-
-"""
-Remove the R or L from after pitcher name
-"""
-def remove_text(cell_value):
-    pattern = r"\s*\(R\)|\s*\(L\)"
-    return re.sub(pattern, "", cell_value)
+    # Add a new column called "KRank"
+    dfopponents2["KRank"] = range(1, len(dfopponents) + 1)
 
 
-dfsched2 = dfsched2.applymap(remove_text)
+    dfopponents2.sort_values("BB%", ascending=True, ignore_index=True, inplace=True)
+    dfopponents2["BBRank"] = range(1, len(dfopponents2) + 1)
 
-# Generate dfopponents again
-dfopponents2 = pd.read_csv("fangraphs-leaderboards (1).csv", usecols=["Team", "K%", "BB%"])
+    dfsched2["Name"] = dfsched2["Name"].str.strip()
+    opposing_teams2 = dfsched2.iloc[:, 1].str.split("\n").str[0]
+    dfsched2["OpposingTeam"] = opposing_teams2
+    dfsched2["OpposingTeam"] = dfsched2["OpposingTeam"].str.replace("@ ", "")
 
-# Update columns in dfopponents2
-# dfopponents2["K%"] = dfopponents2["K%"].str.rstrip("%").astype(float)
-# dfopponents2["BB%"] = dfopponents2["BB%"].str.rstrip("%").astype(float)
+    """
+    Deletes first line in cell from roster resource file
+    """
+    def delete_first_line(cell_value):
+        lines = cell_value.split("\n")
+        if len(lines) > 1:
+            lines.pop(0)
+        return "\n".join(lines)
 
-# Sort and add ranking columns
-dfopponents2.sort_values("K%", ascending=False, ignore_index=True, inplace=True)
-dfopponents2["KRank"] = range(1, len(dfopponents2) + 1)
-dfopponents2.sort_values("BB%", ascending=True, ignore_index=True, inplace=True)
-dfopponents2["BBRank"] = range(1, len(dfopponents2) + 1)
+    """
+    Applies delete_first_line function
+    """
+    dfsched2 = dfsched2.applymap(delete_first_line)
 
-# Update merged_merged_dfsched2 with opponent data again
-dfsched2 = pd.merge(dfsched2, dfopponents2[["Team", "KRank", "BBRank"]], on="Team", how="left")
+    """
+    Remove the R or L from after pitcher name
+    """
+    def remove_text(cell_value):
+        pattern = r"\s*\(R\)|\s*\(L\)"
+        return re.sub(pattern, "", cell_value)
 
-dfsortedBB2 = dfprops2.sort_values("BB%+", ascending=True)
-dfbbtop252 = dfsortedBB2.head(25).fillna(0)
 
-dfsortedK2 = dfprops2.sort_values("K%+", ascending=False)
-dfktop252 = dfsortedK2.head(25).fillna(0)
+    dfsched2 = dfsched2.applymap(remove_text)
 
-matching_dfbbtop252 = dfsched2[dfsched2["Name"].isin(dfbbtop252["Name"])]
-matching_dfktop252 = dfsched2[dfsched2["Name"].isin(dfktop252["Name"])]
+    # Generate dfopponents again
+    dfopponents2 = pd.read_csv( "fangraphs-leaderboards (1).csv", usecols=[ "Team", "K%", "BB%" ] )
 
-matching_dfbbtop252 = dfsched2[
-    (dfsched2["Name"].isin(dfbbtop252["Name"])) & ((dfsched2["BBRank"] <= 10))
-]
-matching_dfktop252 = dfsched2[
-    (dfsched2["Name"].isin(dfktop252["Name"])) & ((dfsched2["KRank"] <= 10))
-]
+    # Update columns in dfopponents2
+    # dfopponents2["K%"] = dfopponents2["K%"].str.rstrip("%").astype(float)
+    # dfopponents2["BB%"] = dfopponents2["BB%"].str.rstrip("%").astype(float)
 
-matching_dfktop252 = pd.merge(
-    matching_dfktop252, dfktop252[["Name", "K%+", "BB%+", "xFIP", "K/GS"]], on="Name", how="left"
-)
-matching_dfbbtop252 = pd.merge(
-    matching_dfbbtop252, dfbbtop252[["Name", "K%+", "BB%+", "xFIP", "BB/GS"]], on="Name", how="left"
-)
+    # Sort and add ranking columns
+    dfopponents2.sort_values("K%", ascending=False, ignore_index=True, inplace=True)
+    dfopponents2["KRank"] = range(1, len(dfopponents2) + 1)
+    dfopponents2.sort_values("BB%", ascending=True, ignore_index=True, inplace=True)
+    dfopponents2["BBRank"] = range(1, len(dfopponents2) + 1)
 
-matching_dfktop252 = matching_dfktop252.sort_values(by="K%+", ascending=False)
-matching_dfbbtop252 = matching_dfbbtop252.sort_values(by="BB%+", ascending=True)
+    # Update merged_merged_dfsched2 with opponent data again
+    dfsched2 = pd.merge(dfsched2, dfopponents2[["Team", "KRank", "BBRank"]], on="Team", how="left")
 
-list_of_dfs = [matching_dfktop252, matching_dfbbtop252, matching_dfktop25, matching_dfbbtop25]
+    dfsortedBB2 = dfprops2.sort_values("BB%+", ascending=True)
+    dfbbtop252 = dfsortedBB2.head(25).fillna(0)
 
-titles = [
-    "Pitcher High Strikeout Props Today",
-    "Pitcher Low BB Props Today",
-    "Pitcher High Strikeout Props Tomorrow",
-    "Pitcher Low BB Props Tomorrow",
-]
+    dfsortedK2 = dfprops2.sort_values("K%+", ascending=False)
+    dfktop252 = dfsortedK2.head(25).fillna(0)
 
-with open("props.csv", "w+", encoding="utf8") as f:
-    for i, df in enumerate(list_of_dfs):
-        if not df.empty:  # Check if the dataframe is not empty
-            f.write(titles[i] + "\n")  # Write the title
-            df.to_csv(f, index=False)
-            f.write("\n")
+    matching_dfbbtop252 = dfsched2[dfsched2["Name"].isin(dfbbtop252["Name"])]
+    matching_dfktop252 = dfsched2[dfsched2["Name"].isin(dfktop252["Name"])]
+
+    matching_dfbbtop252 = dfsched2[
+        (dfsched2["Name"].isin(dfbbtop252["Name"])) & ((dfsched2["BBRank"] <= 10))
+    ]
+    matching_dfktop252 = dfsched2[
+        (dfsched2["Name"].isin(dfktop252["Name"])) & ((dfsched2["KRank"] <= 10))
+    ]
+
+    matching_dfktop252 = pd.merge(
+        matching_dfktop252, dfktop252[["Name", "K%+", "BB%+", "xFIP", "K/GS"]], on="Name", how="left"
+    )
+    matching_dfbbtop252 = pd.merge(
+        matching_dfbbtop252, dfbbtop252[["Name", "K%+", "BB%+", "xFIP", "BB/GS"]], on="Name", how="left"
+    )
+
+    matching_dfktop252 = matching_dfktop252.sort_values(by="K%+", ascending=False)
+    matching_dfbbtop252 = matching_dfbbtop252.sort_values(by="BB%+", ascending=True)
+
+    list_of_dfs = [matching_dfktop252, matching_dfbbtop252, matching_dfktop25, matching_dfbbtop25]
+
+    titles = [
+        "Pitcher High Strikeout Props Today",
+        "Pitcher Low BB Props Today",
+        "Pitcher High Strikeout Props Tomorrow",
+        "Pitcher Low BB Props Tomorrow",
+    ]
+
+    with open("props.csv", "w+", encoding="utf8") as f:
+        for i, df in enumerate(list_of_dfs):
+            if not df.empty:  # Check if the dataframe is not empty
+                f.write(titles[i] + "\n")  # Write the title
+                df.to_csv(f, index=False)
+                f.write("\n")
--- weeklybaseball.py
+++ weeklybaseball.py
@@ -2,8 +2,9 @@ import pandas as pd
 from datetime import date, datetime, timedelta
 import os
 from scipy import stats
+import pandas_log
 
-# # define dictionary
+# define dictionary
 comp_dict = {
     "fangraphs-leaderboards.csv": "fgl_pitchers_10_ip.csv",
     "fangraphs-leaderboards (1).csv": "fgl_pitchers_30_ip.csv",
@@ -21,28 +22,31 @@ for newname, oldname in comp_dict.items():
 
 excluded = pd.read_csv("excluded.csv")
 
-dfhitter = pd.read_csv('hitter.csv')
-dfhitter['Barrel%'] = dfhitter['Barrel%'] = dfhitter['Barrel%'].str.rstrip('%').astype('float64')
-temp_df = dfhitter[['Name', 'playerid']]
-dfhitter = dfhitter.drop(columns = ['playerid', 'mlbamid'])
-columns = ["PA", "HR", "SB", 'BABIP+', 'K%+', 'BB%+', 'ISO+', 'wRC+', 'Barrels', "Barrel%"]
-dfhitter = dfhitter.fillna(0)
-dfhitter[columns] = dfhitter[columns].astype('float')
+with pandas_log.enable():
+    dfhitter = pd.read_csv('hitter.csv')
+    # dfhitter['Barrel%'] = dfhitter['Barrel%'] = dfhitter['Barrel%'].str.rstrip('%').astype('float64')
+    temp_df = dfhitter[['Name', 'PlayerId']]
+    dfhitter = dfhitter.drop(columns = ['PlayerId', 'MLBAMID'])
+    columns = ["PA", "HR", "SB", 'BABIP+', 'K%+', 'BB%+', 'ISO+', 'wRC+', 'Barrels', "Barrel%"]
+    dfhitter = dfhitter.fillna(0)
+    dfhitter[columns] = dfhitter[columns].astype('float')
 
 # Get the list of columns to zscore
 numbers = dfhitter.select_dtypes(include='number').columns
 
-# Zscore the columns
-dfhitter[numbers] = dfhitter[numbers].apply(stats.zscore)
+with pandas_log.enable():
+    # Zscore the columns
+    dfhitter[numbers] = dfhitter[numbers].apply(stats.zscore)
 
-# Add a column for the total z-score
-dfhitter['Total Z-Score'] = pd.Series(dtype=float)
-dfhitter['Total Z-Score'] = dfhitter[numbers].sum(axis=1).round(2)
-dfhitter = dfhitter.merge(temp_df[['Name', 'playerid']], on=["Name"], how="left")
+with pandas_log.enable():
+    # Add a column for the total z-score
+    dfhitter['Total Z-Score'] = pd.Series(dtype=float)
+    dfhitter['Total Z-Score'] = dfhitter[numbers].sum(axis=1).round(2)
+    dfhitter = dfhitter.merge(temp_df[['Name', 'PlayerId']], on=["Name"], how="left")
 
 dfpitcher = pd.read_csv('pitcher.csv')
-temp_df2 = dfpitcher[['Name', 'playerid']]
-dfpitcher = dfpitcher.drop(columns = ['playerid', 'mlbamid'])
+temp_df2 = dfpitcher[['Name', 'PlayerId']]
+dfpitcher = dfpitcher.drop(columns = ['PlayerId', 'MLBAMID'])
 dfpitcher = dfpitcher.fillna(0)
 
 columns2 = ['Stuff+', 'Location+', 'Pitching+', 'Starting', 'Relieving']
@@ -57,7 +61,7 @@ dfpitcher[numbers2] = dfpitcher[numbers2].apply(stats.zscore)
 # Add a column for the total z-score
 dfpitcher['Total Z-Score'] = pd.Series(dtype=float)
 dfpitcher['Total Z-Score'] = dfpitcher[numbers2].sum(axis=1).round(2)
-dfpitcher = dfpitcher.merge(temp_df2[['Name', 'playerid']], on=["Name"], how="left")
+dfpitcher = dfpitcher.merge(temp_df2[['Name', 'PlayerId']], on=["Name"], how="left")
 
 today = date.today()
 today = datetime.strptime(
@@ -66,17 +70,17 @@ today = datetime.strptime(
 
 def hitters_wk_preprocessing(filepath):
     '''Creates weekly hitter calcs'''
-    df = pd.read_csv(filepath, index_col=["playerid"])
+    df = pd.read_csv(filepath, index_col=["PlayerId"])
 
-    df["Barrel%"] = df["Barrel%"] = df["Barrel%"].str.rstrip("%").astype("float")
+    # df["Barrel%"] = df["Barrel%"] = df["Barrel%"].str.rstrip("%").astype("float")
     filter = df[(df["PA"] > 10)]
 
     df.columns = df.columns.str.replace("[+,-,%,]", "", regex=True)
     df.rename(columns={"K%-": "K", "BB%-": "BB"}, inplace=True)
     df.fillna(0)
     df.reset_index(inplace=True)
-    df = df[~df["playerid"].isin(excluded["playerid"])]
-    df = df.merge(dfhitter[["playerid", "Total Z-Score"]], on=["playerid"], how="left")
+    df = df[~df["PlayerId"].isin(excluded["PlayerId"])]
+    df = df.merge(dfhitter[["PlayerId", "Total Z-Score"]], on=["PlayerId"], how="left")
 
     filters = df[
         (df["wRC"] > 135)
@@ -90,17 +94,17 @@ def hitters_wk_preprocessing(filepath):
 
 
 def hitters_pa_preprocessing(filepath):
-    df = pd.read_csv(filepath, index_col=["playerid"])
+    df = pd.read_csv(filepath, index_col=["PlayerId"])
 
-    df["Barrel%"] = df["Barrel%"] = df["Barrel%"].str.rstrip("%").astype("float")
+    # df["Barrel%"] = df["Barrel%"] = df["Barrel%"].str.rstrip("%").astype("float")
     filter = df[(df["PA"] > 10)]
 
     df.columns = df.columns.str.replace("[+,-,%,]", "", regex=True)
     df.rename(columns={"K%-": "K", "BB%-": "BB"}, inplace=True)
     df.fillna(0)
     df.reset_index(inplace=True)
-    df = df[~df["playerid"].isin(excluded["playerid"])]
-    df = df.merge(dfhitter[["playerid", "Total Z-Score"]], on=["playerid"], how="left")
+    df = df[~df["PlayerId"].isin(excluded["PlayerId"])]
+    df = df.merge(dfhitter[["PlayerId", "Total Z-Score"]], on=["PlayerId"], how="left")
 
     filters = df[
         (df["wRC"] > 115)
@@ -115,7 +119,7 @@ def hitters_pa_preprocessing(filepath):
 
 
 def sp_preprocessing(filepath):
-    df = pd.read_csv(filepath, index_col=["playerid"])
+    df = pd.read_csv(filepath, index_col=["PlayerId"])
 
     df.columns = df.columns.str.replace("[+,-,%,]", "", regex=True)
     df.rename(
@@ -123,11 +127,11 @@ def sp_preprocessing(filepath):
     )
     df.fillna(0)
     df.reset_index(inplace=True)
-    df = df[~df["playerid"].isin(excluded["playerid"])]
-    df = df.merge(dfpitcher[["playerid", "Total Z-Score"]], on=["playerid"], how="left")
+    df = df[~df["PlayerId"].isin(excluded["PlayerId"])]
+    df = df.merge(dfpitcher[["PlayerId", "Total Z-Score"]], on=["PlayerId"], how="left")
 
-    df["Barrel"] = df["Barrel"] = df["Barrel"].str.rstrip("%").astype("float")
-    df["CSW"] = df["CSW"] = df["CSW"].str.rstrip("%").astype("float")
+    # df["Barrel"] = df["Barrel"] = df["Barrel"].str.rstrip("%").astype("float")
+    # df["CSW"] = df["CSW"] = df["CSW"].str.rstrip("%").astype("float")
 
     filters1 = df[
         (df["Barrel"] < 7)
@@ -139,7 +143,7 @@ def sp_preprocessing(filepath):
     return filters1
 
 def rp_preprocessing(filepath):
-    df = pd.read_csv(filepath, index_col=["playerid"])
+    df = pd.read_csv(filepath, index_col=["PlayerId"])
 
     df.columns = df.columns.str.replace("[+,-,%,]", "", regex=True)
     df.rename(
@@ -147,11 +151,11 @@ def rp_preprocessing(filepath):
     )
     df.fillna(0)
     df.reset_index(inplace=True)
-    df = df[~df["playerid"].isin(excluded["playerid"])]
-    df = df.merge(dfpitcher[["playerid", "Total Z-Score"]], on=["playerid"], how="left")
+    df = df[~df["PlayerId"].isin(excluded["PlayerId"])]
+    df = df.merge(dfpitcher[["PlayerId", "Total Z-Score"]], on=["PlayerId"], how="left")
 
-    df["Barrel"] = df["Barrel"] = df["Barrel"].str.rstrip("%").astype("float")
-    df["CSW"] = df["CSW"] = df["CSW"].str.rstrip("%").astype("float")
+    # df["Barrel"] = df["Barrel"] = df["Barrel"].str.rstrip("%").astype("float")
+    # df["CSW"] = df["CSW"] = df["CSW"].str.rstrip("%").astype("float")
 
     filters2 = df[
         (df["Barrel"] < 7)
@@ -161,62 +165,62 @@ def rp_preprocessing(filepath):
     ].sort_values(by="Relieving", ascending=False)
     return filters2
 
+with pandas_log.enable():
+    # Preprocess and export the dataframes to Excel workbook sheets
+    hitdaywindow = [7, 14]
+    pawindow = [40]
+    pitchdaywindow = [14, 30]
+    ipwindow = [10, 30]
 
-# Preprocess and export the dataframes to Excel workbook sheets
-hitdaywindow = [7, 14]
-pawindow = [40]
-pitchdaywindow = [14, 30]
-ipwindow = [10, 30]
+    df_list = []
 
-df_list = []
-
-with open("weeklyadds.csv", "w+") as f:
-    for w in hitdaywindow:
-        df = hitters_wk_preprocessing(f"fgl_hitters_last_{w}.csv")
-        df = df.sort_values(by="Total Z-Score", ascending=False)
-        df_list.append(df)
-        if not df.empty:
-            f.write(f"Hitters Last {w} Days\n\n")
-            df.to_csv(f, index=False)
-            f.write("\n")
-
-    for w in pawindow:
-        df = hitters_pa_preprocessing(f"fgl_hitters_{w}_pa.csv")
-        df = df.sort_values(by="Total Z-Score", ascending=False)
-        df_list.append(df)
-        if not df.empty:
-            f.write(f"Hitters {w} PA\n\n")
-            df.to_csv(f, index=False)
-            f.write("\n")
-
-    for w in ipwindow:
-        df = sp_preprocessing(f"fgl_pitchers_{w}_ip.csv")
-        df = df.sort_values(by="Total Z-Score", ascending=False)
-        df_list.append(df)
-        if not df.empty:
-            f.write(f"SP {w} Innings\n\n")
-            df.to_csv(f, index=False)
-            f.write(f"RP {w} Innings\n\n")
-            df = rp_preprocessing(f"fgl_pitchers_{w}_ip.csv")
+    with open("weeklyadds.csv", "w+") as f:
+        for w in hitdaywindow:
+            df = hitters_wk_preprocessing(f"fgl_hitters_last_{w}.csv")
             df = df.sort_values(by="Total Z-Score", ascending=False)
             df_list.append(df)
             if not df.empty:
+                f.write(f"Hitters Last {w} Days\n\n")
+                df.to_csv(f, index=False)
+                f.write("\n")
+
+        for w in pawindow:
+            df = hitters_pa_preprocessing(f"fgl_hitters_{w}_pa.csv")
+            df = df.sort_values(by="Total Z-Score", ascending=False)
+            df_list.append(df)
+            if not df.empty:
+                f.write(f"Hitters {w} PA\n\n")
+                df.to_csv(f, index=False)
+                f.write("\n")
+
+        for w in ipwindow:
+            df = sp_preprocessing(f"fgl_pitchers_{w}_ip.csv")
+            df = df.sort_values(by="Total Z-Score", ascending=False)
+            df_list.append(df)
+            if not df.empty:
+                f.write(f"SP {w} Innings\n\n")
+                df.to_csv(f, index=False)
                 f.write(f"RP {w} Innings\n\n")
-                df.to_csv(f, index=False)
-                f.write("\n")
+                df = rp_preprocessing(f"fgl_pitchers_{w}_ip.csv")
+                df = df.sort_values(by="Total Z-Score", ascending=False)
+                df_list.append(df)
+                if not df.empty:
+                    f.write(f"RP {w} Innings\n\n")
+                    df.to_csv(f, index=False)
+                    f.write("\n")
 
-    for w in pitchdaywindow:
-        df = sp_preprocessing(f"fgl_pitchers_last_{w}.csv")
-        df = df.sort_values(by="Total Z-Score", ascending=False)
-        df_list.append(df)
-        if not df.empty:
-            f.write(f"Pitchers Last {w} Days\n\n")
-            df.to_csv(f, index=False)
-            f.write("\n")
-            df = rp_preprocessing(f"fgl_pitchers_last_{w}.csv")
+        for w in pitchdaywindow:
+            df = sp_preprocessing(f"fgl_pitchers_last_{w}.csv")
             df = df.sort_values(by="Total Z-Score", ascending=False)
             df_list.append(df)
             if not df.empty:
-                f.write(f"RP Last {w} Days\n\n")
+                f.write(f"Pitchers Last {w} Days\n\n")
                 df.to_csv(f, index=False)
                 f.write("\n")
+                df = rp_preprocessing(f"fgl_pitchers_last_{w}.csv")
+                df = df.sort_values(by="Total Z-Score", ascending=False)
+                df_list.append(df)
+                if not df.empty:
+                    f.write(f"RP Last {w} Days\n\n")
+                    df.to_csv(f, index=False)
+                    f.write("\n")
