6bfd1aebeea23ebacd6546b0b9833b423153c121
muneer78 <muneer@duck.com>
2023-09-22 13:44:02 -0500

    Upload new versions of draftsheet

--- baseballdraftsheet.py
+++ baseballdraftsheet.py
@@ -1,141 +1,134 @@
-"""
-Pandas creates and works with the dataframes
-scipy does the z-score calcs
-numpy does some additional stats processing pandas can't do
-"""
 import pandas as pd
 from scipy import stats
 import numpy as np
 
-df = pd.read_csv("pitcher.csv", index_col=["playerid"])  # Preseason Pitchers
-
-expr1 = (
-    (df["GS"] / (df["ER"] * (df["GS"] / df["G"])))
-    .fillna(0)
-    .replace([np.inf, -np.inf], 0)
-)
-expr2 = (df["IP"] * (df["GS"] / df["G"])).fillna(0)
-expr3 = (((df["GS"] + df["G"]) / (2 * df["G"])) ** 2).fillna(0)
-expr4 = expr1 * expr2 * expr3
-df["ER"] = df["ER"] * -1
-df["EstimatedQS"] = expr4
-df = df.drop(["ER", "GS", "G"], axis=1)
-
-df["F-Strike%"] = df["F-Strike%"].str.rstrip("%").astype("float")
-df["Barrel%"] = df["Barrel%"].str.rstrip("%").astype("float")
-df["CSW%"] = df["CSW%"].str.rstrip("%").astype("float")
-df["SwStr%"] = df["SwStr%"].str.rstrip("%").astype("float")
-df["HardHit%"] = df["HardHit%"].str.rstrip("%").astype("float")
-df["Barrel%"] = df["Barrel%"] * -1
-
-numbers = df.select_dtypes(include="number").columns
-df[numbers] = df[numbers].apply(stats.zscore)
-df["SV"] = df["SV"] * 1.5
-df["Barrel%"] = df["Barrel%"] * 1.5
-df["xFIP-"] = df["xFIP-"] * 1.5
-df["SwStr%"] = df["SwStr%"] * 1.5
-df["F-Strike%"] = df["F-Strike%"] * 1.5
-df["CSW%"] = df["CSW%"] * 1.5
-df["K%+"] = df["K%+"] * 5
-df["BB%+"] = df["BB%+"] * 5
-df["EstimatedQS"] = df["EstimatedQS"] * 5
-
-df["Total Z-Score"] = df.sum(axis=1)
-
-rounded_df = df.round(decimals=2).sort_values(by="Total Z-Score", ascending=False)
-
-rounded_df.to_csv("ZPitchers.csv")
-
-df = pd.read_csv("hitter.csv", index_col=["playerid"])  # Preseason Hitters
-
-df["Barrel%"] = df["Barrel%"] = df["Barrel%"].str.rstrip("%").astype("float")
-
-filter1 = df[(df["PA"] > 250) & (df["HR"] > 5)]
-
-numbers = df.select_dtypes(include="number").columns
-df[numbers] = df[numbers].apply(stats.zscore)
-
-df["Total Z-Score"] = df.sum(axis=1)
-
-rounded_df = df.round(decimals=2).sort_values(by="Total Z-Score", ascending=False)
-
-rounded_df.to_csv("ZHitters.csv")
-
-dffgpit = pd.read_csv("fg2.csv")
-dfstuff = pd.read_csv("stuffplus.csv")
-dfadp = pd.read_csv("adp.csv")
-dfzpit = pd.read_csv("ZPitchers.csv")
-dfzhit = pd.read_csv("ZHitters.csv")
-dffghit = pd.read_csv("fg.csv")
-dflaghezza = pd.read_csv("laghezza.csv")
-
-dflist = [dffgpit, dfzpit, dfzhit, dfadp, dfstuff, dffghit, dflaghezza]
-for index, df in enumerate(dflist):
-    df.replace(r"[^\w\s]|_\*", "", regex=True, inplace=True)
-    df.replace(" Jr", "", regex=True, inplace=True)
-    df.replace(" II", "", regex=True, inplace=True)
-
-dfadp = dfadp.astype(str)
-
-
-def func(player_name):
-    """
-    Fix player names in all dataframes
-    """
+# Function to create keys from player names
+def create_key(player_name):
     return "".join([i[:3] for i in player_name.strip().split(" ")])
 
-dffgpit["Key"] = dffgpit.Name.apply(func)
-dfstuff["Key"] = dfstuff.player_name.apply(func)
-dfadp["Key"] = dfadp.Player.apply(func)
-dfzpit["Key"] = dfzpit.Name.apply(func)
-dfzhit["Key"] = dfzhit.Name.apply(func)
-dffghit["Key"] = dffghit.Name.apply(func)
-dflaghezza["Key"] = dflaghezza.Name.apply(func)
+# Read the preseason pitchers data
+df_pitchers = pd.read_csv("pitcher.csv", index_col=["PlayerId"])
 
-dflist2 = [dffgpit, dfzpit, dfadp, dfstuff]
-for index, df in enumerate(dflist):
-    dflist2[index].columns.str.strip()
+# Calculate EstimatedQS
+expr1 = (df_pitchers["GS"] / (df_pitchers["ER"] * (df_pitchers["GS"] / df_pitchers["G"]))).fillna(0).replace([np.inf, -np.inf], 0)
+expr2 = (df_pitchers["IP"] * (df_pitchers["GS"] / df_pitchers["G"])).fillna(0)
+expr3 = (((df_pitchers["GS"] + df_pitchers["G"]) / (2 * df_pitchers["G"])) ** 2).fillna(0)
+df_pitchers["EstimatedQS"] = expr1 * expr2 * expr3
 
-dfadp = dfadp.drop(["ESPN", "CBS", "RTS", "NFBC", "FT"], axis=1)
+# Drop unnecessary columns
+df_pitchers.drop(["ER", "GS", "G"], axis=1, inplace=True)
 
-df1 = (
-    dfadp.merge(dffgpit, on=["Key"], how="left")
-    .merge(
-        dfstuff[["Key", "STUFFplus", "LOCATIONplus", "PITCHINGplus"]],
-        on=["Key"],
-        how="left",
-    )
-    .merge(dfzpit[["Key", "Total Z-Score"]], on=["Key"], how="left")
-    .merge(dfzhit[["Key", "Total Z-Score"]], on=["Key"], how="left")
-    .merge(dffghit, on=["Key"], how="left")
-    .merge(dflaghezza[["Key", "LaghezzaRank"]], on=["Key"], how="left")
-)
-df1 = df1.fillna(value=0)
+# Sum the numerical columns
+numerical_columns = df_pitchers.select_dtypes(include="number").columns
+df_pitchers["Total Numerical Sum"] = df_pitchers[numerical_columns].sum(axis=1)
 
-cols = ["Rank", "LaghezzaRank"]
-df1[cols] = df1[cols] = df1[cols].apply(pd.to_numeric, errors="coerce", axis=1)
+# Apply z-score to numerical columns (excluding the new "Total Numerical Sum" column)
+df_pitchers[numerical_columns] = df_pitchers[numerical_columns].apply(stats.zscore)
 
-df1 = df1.drop_duplicates(subset=["Player", "Rank"], keep="last")
-df1["Total Z-Score"] = df1["Total Z-Score_x"].mask(
-    df1["Total Z-Score_x"].eq(0), df1["Total Z-Score_y"]
-)
-df1["CombinedRank"] = df1["LaghezzaRank"].mask(df1["LaghezzaRank"].eq(0), df1["Rank"])
-df1.loc[(df1.LaghezzaRank.isnull(), "LaghezzaRank")] = df1.Rank
-df1["RankDiff"] = df1["Rank"] - df1["LaghezzaRank"].mask(
-    df1["LaghezzaRank"].eq(0), df1["Rank"]
-)
-df1 = df1.rename(columns={"Rank": "ADP"})
-df1.to_csv("fulldraftsheet.csv")
+# Modify specific columns
+df_pitchers["SV"] *= 1.5
+df_pitchers["Barrel%"] *= 1.5
+df_pitchers["xFIP-"] *= 1.5
+df_pitchers["SwStr%"] *= 1.5
+df_pitchers["F-Strike%"] *= 1.5
+df_pitchers["CSW%"] *= 1.5
+df_pitchers["K%+"] *= 5
+df_pitchers["BB%+"] *= 5
+df_pitchers["EstimatedQS"] *= 5
 
-df2 = pd.read_csv("fulldraftsheet.csv")
+# Calculate Total Z-Score
+df_pitchers["Total Z-Score_Pitcher"] = df_pitchers[numerical_columns].sum(axis=1)
 
-columns = ["Player", "Team_x", "Total Z-Score", "ADP", "LaghezzaRank", "RankDiff"]
-df2 = pd.DataFrame(df2, columns=columns)
-df2 = df2.rename(columns={"Team_x": "Team"})
-df2["ADP"] = sorted(df2["ADP"], key=float)
-df2.to_csv("draftsheet.csv")
+# Drop the intermediate "Total Numerical Sum" column if needed
+df_pitchers.drop("Total Numerical Sum", axis=1, inplace=True)
 
-df3 = pd.read_csv("draftsheet.csv", index_col=False)
-columns = ["Player", "Team"]
-df3 = pd.DataFrame(df3, columns=columns)
-df3.to_csv("fantrax.csv")
+# Round the dataframe and save to CSV
+rounded_df_pitchers = df_pitchers.round(decimals=2).sort_values(by="Total Z-Score_Pitcher", ascending=False)
+rounded_df_pitchers.to_csv("ZPitchers.csv")
+
+# Read the preseason hitters data
+df_hitters = pd.read_csv("hitter.csv", index_col=["PlayerId"])
+
+# Filter the hitters dataframe
+filter1 = df_hitters[(df_hitters["PA"] > 250) & (df_hitters["HR"] > 5)]
+
+# Apply z-score to numerical columns
+numerical_columns1 = df_hitters.select_dtypes(include="number").columns
+
+# Sum the numerical columns
+df_hitters["Total Numerical Sum"] = df_hitters[numerical_columns1].sum(axis=1)
+
+df_hitters[numerical_columns1] = df_hitters[numerical_columns1].apply(stats.zscore)
+
+# Calculate Total Z-Score
+df_hitters["Total Z-Score_Hitter"] = df_hitters[numerical_columns1].sum(axis=1)
+
+# Drop the intermediate "Total Numerical Sum" column if needed
+df_hitters.drop("Total Numerical Sum", axis=1, inplace=True)
+
+# Round the dataframe and save to CSV
+rounded_df_hitters = df_hitters.round(decimals=2).sort_values(by="Total Z-Score_Hitter", ascending=False)
+rounded_df_hitters.to_csv("ZHitters.csv")
+
+# Read the additional dataframes
+df_adp = pd.read_csv("FantasyPros_Fantasy_Baseball_Rankings_ALL.csv")
+df_zpit = pd.read_csv("ZPitchers.csv")
+df_zhit = pd.read_csv("ZHitters.csv")
+df_laghezza = pd.read_csv("TOP 625 3.9 UPDATE.csv")
+
+df_adp = df_adp.rename(columns={"Player": "Name"})
+
+# Clean player names in all dataframes
+dataframes_to_clean = [df_adp, df_zpit, df_zhit, df_laghezza]
+for df in dataframes_to_clean:
+    df["Name"] = df["Name"].str.replace(r"[^\w\s]|_\*", "").str.replace(" Jr", "").str.replace(" II", "")
+
+# Convert df_adp to string
+df_adp = df_adp.astype(str)
+
+# Rename the "Rank" column to "ADP"
+df_laghezza = df_laghezza.rename(columns={"PosRank": "LRank"})
+
+# Apply the create_key function to add the "Key" column
+df_adp["Key"] = df_adp["Name"].apply(create_key)
+df_zpit["Key"] = df_zpit["Name"].apply(create_key)
+df_zhit["Key"] = df_zhit["Name"].apply(create_key)
+df_laghezza["Key"] = df_laghezza["Name"].apply(create_key)
+
+# Create a list of dataframes for merging
+dflist = [df_zpit, df_zhit, df_adp, df_laghezza]
+
+# Merge the dataframes with suffixes to handle duplicate columns
+df1 = df_adp.merge(df_zpit[["Key", "Total Z-Score_Pitcher"]], on=["Key"], how="left")\
+    .merge(df_zhit[["Key", "Total Z-Score_Hitter"]], on=["Key"], how="left")\
+    .merge(df_laghezza[["Key", "LRank"]], on=["Key"], how="left")
+
+# Fill NaN values with zeros
+df1 = df1.fillna(0)
+
+# Convert specific columns to numeric
+cols = ["Rank"]
+df1[cols] = df1[cols].apply(pd.to_numeric, errors="coerce", axis=1)
+
+# Drop duplicates based on "Player" and "Rank" columns
+df1 = df1.drop_duplicates(subset=["Name", "Rank"], keep="last")
+
+# Calculate the "Total Z-Score" column
+df1["Total Z-Score"] = df1["Total Z-Score_Pitcher"].mask(df1["Total Z-Score_Pitcher"].eq(0), df1["Total Z-Score_Hitter"])
+
+# Create the "CombinedRank" column
+df1["CombinedRank"] = df1["LRank"].mask(df1["LRank"].eq(0), df1["Rank"])
+df1.loc[df1.LRank.isnull(), "LRank"] = df1["Rank"]
+
+# Calculate the "RankDiff" column
+df1["RankDiff"] = df1["Rank"] - df1["LRank"].mask(df1["LRank"].eq(0), df1["Rank"])
+
+# Select and rename columns
+columns = ["Name", "Team", "Total Z-Score", "RankDiff", "ADP"]
+df1 = df1[columns]
+
+# Sort the "ADP" column
+df1["ADP"] = sorted(df1["ADP"], key=float)
+
+# Save the second dataframe to CSV
+df1.to_csv("draftsheet.csv")
